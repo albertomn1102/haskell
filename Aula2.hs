96586ed3c062907a21dba4144315c9b561dcc2a9
albertomn1102 <32913970+albertomn1102@users.noreply.github.com>
2017-10-18 20:44:30 -0200

    Delete Aula2.hs

--- Aula2.hs
+++
@@ -1,9 +0,0 @@
-module Aula2 where
-
-import Data.List
-
-
-faznada x = (head x) : (tail x)
-
---tail pega toda a lista(string) menos a primeira letra
--- : concatenando (ele so concatena um elemento com uma lista)
